c5d49811eb94b44c2862b35dd348cc0f9a59ed35
chunjaeilu <chunjaeilu@naver.com>
2023-02-22 17:47:47 +0900

    project 컨텐츠 채우는중

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,14 +12,19 @@ export default function Projects() {
   const projectData = [
     {
       title: "DamDam",
+      category: "Team Project",
       content: "Content 1",
+      img: "./images/project1.png",
+      desc: "",
     },
     {
       title: "Kolon Global",
+      category: "Individual Project",
       content: "Content 2",
     },
     {
-      title: "My Public Service",
+      title: "Public Service",
+      category: "Individual Project",
       content: "Content 3",
     },
   ];
@@ -27,18 +32,18 @@ export default function Projects() {
     <section>
       <h2>PROJECTS</h2>
       <div className="project-box">
-        {projectData.map((item, index) => (
-          <div key={index} className="project">
+        {projectData.map((item, i) => (
+          <div key={i} className="project">
             <div
               className="btn-arcodion"
               onClick={() => {
-                setActiveIndex(index);
+                setActiveIndex(i);
               }}
             >
-              {item.title}
+              <p>{item.title}</p>
             </div>
-            <div className={`content ${activeIndex === index ? "active" : ""}`}>
-              <Project title={item.title} content={item.content} />
+            <div className={`content ${activeIndex === i ? "active" : ""}`}>
+              <Project item={item} />
             </div>
           </div>
         ))}
